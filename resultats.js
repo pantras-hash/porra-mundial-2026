7e226965a7827becfb5fd610b820abe6443430b0
pantras-hash <pantras@fas.harvard.edu>
2026-06-11 22:39:05 -0400

    Update resultats.js

--- resultats.js
+++ resultats.js
@@ -13,7 +13,7 @@ window.PORRA_RESULTATS = {
     // Mexico vs South Africa
     "G-A-1": { homeScore: 2, awayScore: 0, penHome: null, penAway: null, date: "2026-06-11", sortOrder: 1 },
     // Korea Republic vs Czechia
-    "G-A-2": { homeScore: null, awayScore: null, penHome: null, penAway: null, date: "2026-06-11", sortOrder: 2 },
+    "G-A-2": { homeScore: 1, awayScore: 1, penHome: null, penAway: null, date: "2026-06-11", sortOrder: 2 },
     // 2026-06-12
     // Canada vs Bosnia-Herzegovina
     "G-B-1": { homeScore: null, awayScore: null, penHome: null, penAway: null, date: "2026-06-12", sortOrder: 3 },
