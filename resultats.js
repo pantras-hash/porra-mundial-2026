1761e702ceb73fbc0fc1127c812e2413b366d0e2
pantras-hash <pantras@fas.harvard.edu>
2026-06-11 22:27:11 -0400

    Update resultats.js

--- resultats.js
+++ resultats.js
@@ -11,7 +11,7 @@ window.PORRA_RESULTATS = {
     // Mexico vs South Africa
     "G-A-1": { homeScore: 2, awayScore: 0, penHome: null, penAway: null },
     // Korea Republic vs Czechia
-    "G-A-2": { homeScore: 1, awayScore: 1, penHome: null, penAway: null },
+    "G-A-2": { homeScore: null, awayScore: null, penHome: null, penAway: null },
     // Mexico vs Korea Republic
     "G-A-3": { homeScore: null, awayScore: null, penHome: null, penAway: null },
     // Czechia vs South Africa
